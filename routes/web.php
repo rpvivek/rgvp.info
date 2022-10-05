275af86575b8d4facce14986b037d23fc5eac7cf
Vivek Pandharkar <rpvivek@websiteinnagpur.com>
2022-10-05 16:33:20 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('welcome');
+});
+//Direct Route
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route using controller in laravel 6 and 7;
+//Route::get('/contact', "ContactController@index");
+
+// Route using controller in laravel 8;
+
+Route::get('/contact', [ContactController::class,'index'])->middleware('authpage');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
